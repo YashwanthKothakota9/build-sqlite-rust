c3b5f33205fee8995208f1bc01c9233b14cccf64
YashwanthK <yashwanthkothakota@gmail.com>
2025-06-25 20:07:58 +0530

    add: print number of tables

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,13 @@ fn main() -> Result<()> {
             #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
+            let mut root_page_header = [0; 8];
+            file.read_exact(&mut root_page_header)?;
+
+            let num_tables = u16::from_be_bytes([root_page_header[3], root_page_header[4]]);
+
             println!("database page size: {}", page_size);
+            println!("number of tables: {}", num_tables);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
