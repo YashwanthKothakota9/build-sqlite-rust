275ce7be487c11ef1f50500b6477ed39b8264704
YashwanthK <yashwanthkothakota@gmail.com>
2025-06-25 22:05:42 +0530

    add: print table names

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-use anyhow::{bail, Result};
-use std::fs::File;
-use std::io::prelude::*;
+mod sqlite;
 
-fn main() -> Result<()> {
-    // Parse arguments
-    let args = std::env::args().collect::<Vec<_>>();
+use std::env;
+
+use anyhow::bail;
+
+use sqlite::{Database, RecordValue};
+
+fn main() -> anyhow::Result<()> {
+    let args = env::args().collect::<Vec<_>>();
     match args.len() {
         0 | 1 => bail!("Missing <database path> and <command>"),
         2 => bail!("Missing <command>"),
         _ => {}
     }
+    let (db_path, command) = (&args[1], &args[2]);
 
-    // Parse command and act accordingly
-    let command = &args[2];
+    let db = Database::load(db_path)?;
+
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
+            println!("database page size: {}", db.page_size);
+            println!("number of tables: {}", db.root_page.cell_pointers.len());
+        }
+        ".tables" => {
+            let table_names: Vec<_> = db
+                .root_page
+                .records()
+                .map(|r| match &r.values[2] {
+                    RecordValue::Text(s) => s.clone(),
+                    _ => panic!("Expected table name, got {:?}", r.values[0]),
+                })
+                .collect();
+            let table_names = table_names.join(" ");
 
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
-
-            let mut root_page_header = [0; 8];
-            file.read_exact(&mut root_page_header)?;
-
-            let num_tables = u16::from_be_bytes([root_page_header[3], root_page_header[4]]);
-
-            println!("database page size: {}", page_size);
-            println!("number of tables: {}", num_tables);
+            println!("{}", table_names);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
---
+++ src/sqlite/db.rs
@@ -0,0 +1,155 @@
+use std::{fs::File, io::Read};
+
+const DB_HEADER_SIZE: usize = 100;
+
+#[derive(Debug, Clone)]
+pub enum RecordValue {
+    Null,
+    Int(i64),
+    Real(f64),
+    Text(String),
+    Blob(Vec<u8>),
+}
+
+#[derive(Debug)]
+pub struct Record {
+    pub id: u64,
+    pub values: Vec<RecordValue>,
+}
+
+#[derive(Debug)]
+enum PageType {
+    TableLeaf,
+}
+
+#[derive(Debug)]
+pub struct Page {
+    pub typ: PageType,
+    pub cell_pointers: Vec<usize>,
+    data: Vec<u8>,
+}
+
+impl Page {
+    fn from_data(page_size: u16, data: Vec<u8>) -> Self {
+        let typ = match data[0] {
+            13 => PageType::TableLeaf,
+            _ => panic!("Invalid page type: {}", data[0]),
+        };
+        let cell_count = u16::from_be_bytes([data[3], data[4]]);
+        let data_offset = 8 + (cell_count as usize * 2);
+        let offset = (page_size as usize) - data.len() + data_offset;
+        let cell_pointers = data[8..data_offset]
+            .chunks(2)
+            .map(|chunk| u16::from_be_bytes([chunk[0], chunk[1]]) as usize - offset)
+            .collect();
+        let data = data[8 + (cell_count as usize * 2)..].to_vec();
+
+        Self {
+            typ,
+            cell_pointers,
+            data,
+        }
+    }
+
+    fn get_varint(data: &[u8], offset: &mut usize) -> u64 {
+        let mut value = 0u64;
+        for (i, byte) in data[*offset..].iter().enumerate() {
+            value <<= 7;
+            value += (byte & 0x7F) as u64;
+            if byte & 0x80 == 0 {
+                *offset += i + 1;
+                return value;
+            }
+        }
+        *offset += data.len();
+        value
+    }
+
+    fn get_be_bytes(n: usize, data: &mut impl Iterator<Item = u8>) -> [u8; 8] {
+        let mut bytes = [0; 8];
+        for i in 0..n {
+            bytes[8 - n + i] = data.next().unwrap();
+        }
+        bytes
+    }
+
+    fn get_record(&self, pointer: usize) -> Record {
+        // TODO: Handle varints with size > 1 byte
+        let mut offset = pointer;
+        let size = Self::get_varint(&self.data, &mut offset) as usize;
+        let id = Self::get_varint(&self.data, &mut offset) as u64;
+        let header_size = Self::get_varint(&self.data, &mut offset) as usize;
+
+        let header_boundary = offset + header_size - 1;
+        let end_boundary = pointer + size + 2;
+        let mut data = self.data[header_boundary..end_boundary].iter().copied();
+
+        let mut values = Vec::with_capacity(header_size);
+        let mut header_offset = 0;
+
+        while header_offset < header_size - 1 {
+            let typ = Self::get_varint(&self.data[offset..header_boundary], &mut header_offset);
+            let value = match typ {
+                0 => RecordValue::Null,
+                1..=6 => {
+                    let n = match typ {
+                        5 => 6,
+                        6 => 8,
+                        v => v,
+                    } as usize;
+                    let value = i64::from_be_bytes(Self::get_be_bytes(n, &mut data));
+                    RecordValue::Int(value)
+                }
+                7 => {
+                    let value = f64::from_be_bytes(Self::get_be_bytes(8, &mut data));
+                    RecordValue::Real(value)
+                }
+                8 => RecordValue::Int(0),
+                9 => RecordValue::Int(1),
+                _ if typ >= 12 => {
+                    let length = ((typ - 12) / 2) as usize;
+                    let value = (&mut data).take(length).collect::<Vec<u8>>();
+                    if typ % 2 == 0 {
+                        RecordValue::Blob(value)
+                    } else {
+                        let value = String::from_utf8_lossy(&value).to_string();
+                        RecordValue::Text(value)
+                    }
+                }
+                _ => panic!("Invalid value type: {}", typ),
+            };
+            values.push(value);
+        }
+
+        Record { id, values }
+    }
+
+    pub fn records(&self) -> impl Iterator<Item = Record> + '_ {
+        self.cell_pointers.iter().map(|i| self.get_record(*i))
+    }
+}
+
+#[derive(Debug)]
+pub struct Database {
+    pub page_size: u16,
+    pub root_page: Page,
+}
+
+impl Database {
+    pub fn load(path: &str) -> anyhow::Result<Self> {
+        let mut file = File::open(path)?;
+
+        let mut db_header = [0; DB_HEADER_SIZE];
+        file.read_exact(&mut db_header)?;
+        let page_size = u16::from_be_bytes([db_header[16], db_header[17]]);
+
+        let mut root_page = vec![0; page_size as usize - DB_HEADER_SIZE];
+        file.read_exact(&mut root_page)?;
+        let root_page = Page::from_data(page_size, root_page);
+
+        Ok(Self {
+            page_size,
+            root_page,
+        })
+    }
+}
